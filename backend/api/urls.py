66b957159fd77fa9c68d85c73e6ead49b47e2f41
Sakki200 <neil.baldeck@gmail.com>
2025-10-26 23:18:05 +0100

    fix(urls) : fix path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("auth/validation/<uuid:id>/", user_views.ValidationActivateView.as_view(), name="validation"),
     path("auth/password/", user_views.PasswordPatchView.as_view(), name="password_patch"),
     path("canva/create/", canva_views.CanvaCreateView.as_view(), name="canva_create"),
-    path("canva/modification/", canva_views.CanvaUserView.as_view(), name="canva_user"),
+    path("canva/user/", canva_views.CanvaUserView.as_view(), name="canva_user"),
 ]
