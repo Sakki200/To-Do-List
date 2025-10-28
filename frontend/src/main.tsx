db386ced6b7bceebe657bc2cc0a3e6840f5887d0
Sakki200 <neil.baldeck@gmail.com>
2025-10-28 06:03:05 +0100

    feat(setup) : Install and Setup React
    React is installed in frontend new folder.
    React router dom is setup.
    Tailwindcss is installed and setup.

---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import "./css/index.css";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
